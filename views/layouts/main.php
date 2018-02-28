0217a349222eaf46dd1373e59cf7cbc61350d5d0
Ing. Oscar Salas <ing.olsalas@gmail.com>
2018-02-27 23:13:08 -0500

    Agregando Plugins de Terceros

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -58,7 +58,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Servicios TJ <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
